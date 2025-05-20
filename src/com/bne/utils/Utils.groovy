cf0294c03d0a74aa8c46f35702cbeb036fe77c9c
liujun012033 <316828252@qq.com>
2025-05-20 13:43:59 +0000

    test

--- src/com/bne/utils/Utils.groovy
+++ src/com/bne/utils/Utils.groovy
@@ -6,3 +6,4 @@ class Utils {
         script.echo 'Hello from shared library!'
     }
 }
+
---
+++ vars/deploy.groovy
@@ -0,0 +1,5 @@
+// vars/deploy.groovy
+def call(Map args) {
+    echo "Deploying ${args.appName} to ${args.environment}"
+    // 执行部署逻辑
+}
